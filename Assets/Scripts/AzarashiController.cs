88f7b368a98603b8c0c90a47d9eaa76f285085ee
yukari.haneda0421 <yukari.haneda0421@gmail.com>
2023-02-15 11:02:20 +0900

    p371まで作成

--- Assets/Scripts/AzarashiController.cs
+++ Assets/Scripts/AzarashiController.cs
@@ -41,22 +41,39 @@ public class AzarashiController : MonoBehaviour
         ApplyAngle();
 
         //angleが水平以上だったら、アニメーターのflapフラグをtrueにする
-        animator.SetBool("flap", angle >= 0.0f);
+        animator.SetBool("flap", angle >= 0.0f && !isDead);
     }
 
     public void Flap() {
+        //死んだらはばたけない
+        if (isDead) return;
+
         // Velocityを直接置き換えて上方向に加速
         rb2d.velocity = new Vector2(0.0f, flapVelocity);
     }
 
     void ApplyAngle() {
         //現在の速度、相対速度から進んでいる角度を求める
-        float targetAngle = Mathf.Atan2(rb2d.velocity.y, relativeVelocityX) * Mathf.Rad2Deg;
+        float targetAngle;
+
+        //死亡したら常にひっくりかえる
+        if(isDead) {
+            targetAngle = 180.0f;
+        }else {
+            targetAngle = Mathf.Atan2(rb2d.velocity.y, relativeVelocityX) * Mathf.Rad2Deg;
+        }
 
         //回転アニメをスムージング
         angle = Mathf.Lerp(angle, targetAngle, Time.deltaTime * 10.0f);
 
         //Rotationの反映
         sprite.transform.localRotation = Quaternion.Euler(0.0f, 0.0f, angle);
+    }
+
+    void OnCollisionEnter2D(Collision2D collision) {
+        if(isDead) return;
+
+        //何かぶつかったら死亡フラグをたてる
+        isDead = true;
     }
 }
